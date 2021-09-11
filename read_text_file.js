09efbd228df28f6090c1ea52bced1b110bdcfd60
Trevor Carson <78237159+Trevor-Carson@users.noreply.github.com>
2021-09-11 15:52:11 -0400

    Add files via upload

--- read_text_file.js
+++ read_text_file.js
@@ -5,14 +5,14 @@
 *** Date:           2021-09-11
 *****************************************************************************************************/
 
-const fs = require('fs');
+const fs = require('fs');   // Includes the File System module for Node.js
 
 let readDataCallback = (err, data) => {
-  if (err) {
-    console.log(`Something went wrong: ${err}`);
-  } else {
-    console.log(`Provided file contained: ${data}`);
-  }
+    if (err) {
+        console.log(`Something went wrong: ${err}`);
+    } else {
+        console.log(`Provided file contained: ${data}`);
+    }
 };
 
 fs.readFile("./test.txt", "utf-8", readDataCallback);
